b430cd7ffbf3f855d033c63ab2bf29716bf5769d
wu-yiqiang <wu_yiqiang@outlook.com>
2022-09-02 21:19:24 +0800

    初始化

---
+++ LeeCode/Go/main/main.go
@@ -0,0 +1,37 @@
+package main
+import (
+	"fmt"
+)
+
+func main() {
+	fmt.Println(longestPalindrome("adsssssssss"))
+}
+
+
+func longestPalindrome(s string) string {
+	if len(s) == 1 {
+		return s
+	}
+	var str string
+	if len(s) == 2 && s[0:1] == s[1:2] {
+		return s
+	}
+	str = string(s[0:1])
+	for i := 0; i < len(s); i++ {
+		for j := len(s) - 1; j >= i; j++ {
+			if(isPalin(s[i:j]) && j - i > len(str) ) {
+				str = s[i:j]
+			}
+		}
+ 	}
+	return str
+}
+
+func isPalin(str string) bool{
+	for r := 0, k := len(str) - 1; r < len(str), k >= r; r++, k-- {
+		if (byte(str[r]) != byte(str[k])) {
+			return false
+		}
+	}
+	return true
+}
